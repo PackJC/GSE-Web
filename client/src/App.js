cbabeb0c075fbd8f3534f0138d8c6ac373d43623
Johnny Pack <packjc@etsu.edu>
2021-08-19 21:09:38 -0400

    .
    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {DataProvider} from './GlobalState'
 import Header from './components/headers/Header'
+
 import MainPages from './components/mainpages/Pages'
 
 
