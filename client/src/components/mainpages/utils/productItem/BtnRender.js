2986cec1c74472a85a22ad5901ed5f3eb2382e88
Johnny Pack <34726562+PackJC@users.noreply.github.com>
2021-08-28 19:28:35 -0400

    Update BtnRender.js

--- client/src/components/mainpages/utils/productItem/BtnRender.js
+++ client/src/components/mainpages/utils/productItem/BtnRender.js
@@ -8,38 +8,55 @@ function BtnRender({product, deleteProduct}) {
     const addCart = state.userAPI.addCart
     const product = state.userAPI.product
 
-    return (
-        <div className="row_btn">
-            {
-                isAdmin ?
-                <>
-                    <Link id="btn_del" to="#!"
-                    onClick={() =>deleteProduct(product._id, product.images.public_id)}>
-                        Delete
-                    </Link>
-                    <Link id="btn_view" to={`/edit_product/${product._id}`}>
-                        Edit
-                    </Link>
-                </>
-                : <>
-                }
-                if(product.stock === 0){
+    if(product.stock < 1){
+      return (
+          <div className="row_btn">
+              {
+                  isAdmin ?
+                  <>
+                      <Link id="btn_del" to="#!"
+                      onClick={() =>deleteProduct(product._id, product.images.public_id)}>
+                          Delete
+                      </Link>
+                      <Link id="btn_view" to={`/edit_product/${product._id}`}>
+                          Edit
+                      </Link>
+                  </>
+                  :
+                  <>
                   <button type="button" disabled className="cartButtonDisabled">
                       OUT OF STOCK
                   </button>
-                }
-                else{
-                  <Link id="btn_buy" to="#!" onClick={() => addCart(product)}>
-                      Add To Cart
-                  </Link>
-                }
+                  </>
+              }
 
+          </div>
+      )
+    }
+    else{
+      return (
+          <div className="row_btn">
+              {
+                  isAdmin ?
+                  <>
+                      <Link id="btn_del" to="#!"
+                      onClick={() =>deleteProduct(product._id, product.images.public_id)}>
+                          Delete
+                      </Link>
+                      <Link id="btn_view" to={`/edit_product/${product._id}`}>
+                          Edit
+                      </Link>
+                  </>
+                  :
+                  <>
+                      <Link id="btn_buy" to="#!" onClick={() => addCart(product)}>
+                          Add To Cart
+                      </Link>
+                  </>
+              }
 
-                </>
-            }
-
-        </div>
-    )
+          </div>
+      )
+  }
 }
-
 export default BtnRender
